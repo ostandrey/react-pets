39403f29cc1f3114fb9bf17945d32435f8f4320c
ostandrey <ost.and.g@gmail.com>
2022-07-22 18:06:30 +0300

    Create home page, add styles

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import classes from './Navigation.module.css'
+
+const Navigation = () => {
+    return (
+        <div className={classes.container}>
+            <header>
+                <img src={require("../../assets/Logo.png")} alt="Logo"/>
+            </header>
+            <section className={classes.title_container}>
+                <h1 className={classes.title}>Hi intern!</h1>
+                <h3 className={classes.subtitle}>Welcome to MI 2022 Front-end test</h3>
+            </section>
+            <section className={classes.links_container}>
+                <h3 className={classes.title_sm}>Lets start using The Cat API</h3>
+                <div className={classes.nav_buttons}>
+                    <div className={classes.nav_container}>
+                        <div className={[classes.nav_card, classes.card_voting].join(' ')}>
+                            <img src={require("../../assets/vote-table.png")} alt="vote-table"/>
+                        </div>
+                        <div className={classes.nav_btn}>
+                            <h2 className={classes.btn_title}>
+                                Voting
+                            </h2>
+                        </div>
+                    </div>
+                    <div className={classes.nav_container}>
+                        <div className={[classes.nav_card, classes.card_breeds].join(' ')}>
+                            <img src={require("../../assets/pet-breeds.png")} alt="pet-breeds"/>
+                        </div>
+                        <div className={classes.nav_btn}>
+                            <h2 className={classes.btn_title}>
+                                Breeds
+                            </h2>
+                        </div>
+                    </div>
+                    <div className={classes.nav_container}>
+                        <div className={[classes.nav_card, classes.card_gallery].join(' ')}>
+                            <img src={require("../../assets/images-search.png")} alt="images-search"/>
+                        </div>
+                        <div className={classes.nav_btn}>
+                            <h2 className={classes.btn_title}>
+                                Gallery
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Navigation;
