c3fc2f8cbdcdcdb51aa799891578d4be6f50ed20
ostandrey <ost.and.g@gmail.com>
2022-07-26 19:19:22 +0300

    Display images. Edit styles

--- src/components/BreedItem/BreedItem.jsx
+++ src/components/BreedItem/BreedItem.jsx
@@ -3,17 +3,18 @@ import classes from "./BreedsItem.module.css";
 
 const BreedItem = ({dog, ...props}) => {
     return (
-        <>
-            {/**/}
-            <div className={classes.container}>
-                {dog.name}
-                {/*<img src={dog.image.url} alt="dog" className={classes.image}/>*/}
-            </div>
-            {/*<div className={classes.div2}></div>*/}
-            {/*<div className={classes.div3}></div>*/}
-            {/*<div className={classes.div4}></div>*/}
-            {/*<div className={classes.div5}></div>*/}
-        </>
+        <div className={classes.container}>
+            {/*{dog.name}*/}
+            {
+                dog && <img src={`${dog.image.url}`} alt="dog" className={classes.image}/>
+            }
+
+        </div>
+            // {/*<div className={classes.div2}></div>*/}
+            // {/*<div className={classes.div3}></div>*/}
+            // {/*<div className={classes.div4}></div>*/}
+            // {/*<div className={classes.div5}></div>*/}
+
     );
 };
 
