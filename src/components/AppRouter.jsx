efd8e3368cd072884ffaf5c55388479aaaea59dc
ostandrey <ost.and.g@gmail.com>
2022-08-03 20:29:42 +0300

    Edit routing

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -8,20 +8,23 @@ import BreedIdPage from "../pages/BreedIdPage/BreedIdPage";
 import Favourites from "../pages/Favourites/Favourites";
 import Likes from "../pages/Likes/Likes";
 import Dislikes from "../pages/Dislikes/Dislikes";
+import Layout from "./Layout";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route exact path="/home"  element={<Home />}/>
-            <Route exact path='/voting' element={<Voting />}/>
-            <Route exact path="/breeds"  element={<Breeds />}/>
-            <Route exact path="/breeds/:name"  element={<BreedIdPage />}/>
-            <Route exact path="/gallery"  element={<Gallery />}/>
-            <Route path="/favourites" element={<Favourites />}/>
-            <Route path="/likes" element={<Likes />}/>
-            <Route path="/dislikes" element={<Dislikes />}/>
-            {/*<Route path="/error"  element={<Error />}/>*/}
             <Route path='*' element={<Navigate replace to="/home" />}/>
+            <Route exact path="/"  element={<Layout />}>
+                <Route index path='/voting' element={<Voting />}/>
+                <Route exact path="/breeds"  element={<Breeds />}/>
+                <Route exact path="/breeds/:name"  element={<BreedIdPage />}/>
+                <Route exact path="/gallery"  element={<Gallery />}/>
+                <Route path="/favourites" element={<Favourites />}/>
+                <Route path="/likes" element={<Likes />}/>
+                <Route path="/dislikes" element={<Dislikes />}/>
+                {/*<Route path="/error"  element={<Error />}/>*/}
+            </Route>
         </Routes>
     );
 };
