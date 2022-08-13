ec68a6333d0e297fd0764733696986a1278a7802
ostandrey <ost.and.g@gmail.com>
2022-08-13 12:38:24 +0300

    Update modal styles

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -36,24 +36,29 @@ const Gallery = () => {
                     <img src={require('../../assets/upload.png')} alt="upload" />
                     Upload
                 </MyButton>
-                <MyModal visible={modal} setVisible={setModal}>
+                <MyModal visible={modal} setVisible={setModal} onClick={() => setModal(false)}>
                     <MyButton className={classes.close_btn} onClick={() => setModal(false)}>
                         <img src={require('../../assets/close.png')} alt="close" />
                     </MyButton>
-                    <h1>Upload a .jpg or .png Cat Image</h1>
-                    <h3>Any uploads must comply with the
-                        <a href="https://thecatapi.com/privacy" target="_blank">upload guidelines</a>
-                        or face deletion.
-                    </h3>
+                    <div className={classes.title_block}>
+                        <h1 className={classes.modal_title}>Upload a .jpg or .png Cat Image</h1>
+                        <h3 className={classes.modal_subtitle}>Any uploads must comply with the
+                            <a href="https://thecatapi.com/privacy"
+                               target="_blank"
+                               className={classes.text_link}
+                            > upload guidelines </a>
+                            or face deletion.
+                        </h3>
+                    </div>
                     <form method="post" action="#" onSubmit={onSubmit} className={classes.files}>
                             <input type="file" multiple='' onChange={e => setFile(e.target.files[0])}/>
                             {
                                 !file && <>
                                     <img src={require('../../assets/not-found-image.png')} alt="close" />
                                     <p>
-                                        <span>Drag here</span>
+                                        <span>Drag here </span>
                                         your file or
-                                        <span>Click here</span>
+                                        <span> Click here </span>
                                         to upload
                                     </p>
                                 </>
@@ -63,13 +68,13 @@ const Gallery = () => {
                             }
                     </form>
                     {
-                        !file && <h3>No file selected</h3>
+                        !file && <h3 className={classes.modal_subtitle}>No file selected</h3>
                     }
                     {
-                        file && <>
-                            <h3>Image File Name: {file.name}</h3>
+                        file && <div className={classes.upload_actions}>
+                            <h3 className={classes.modal_subtitle}>Image File Name: {file.name}</h3>
                             <MyButton className={classes.upload_btn}>Upload photo</MyButton>
-                        </>
+                        </div>
                     }
                 </MyModal>
             </div>
