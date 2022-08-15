63a2ebf9bc3ddcb1c751cad38438c115136c269d
ostandrey <ost.and.g@gmail.com>
2022-08-15 23:50:14 +0300

    Update gallery page

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,17 +1,40 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./Gallery.module.css";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import MyButton from "../../components/UI/MyButton";
 import MyModal from "../../components/UI/modal/MyModal";
 import {useFetching} from "../../hooks/useFetching";
 import DogService from "../../API/DogService";
-import {logDOM} from "@testing-library/react";
+import Loader from "../../components/UI/Loader/Loader";
 
 const Gallery = () => {
     const [modal, setModal] = useState(false);
 
     const [file, setFile] = useState(null);
     const [dataFile, setDataFile] = useState(null);
+    const [uploadedImages, setUploadedImages] = useState(null);
+    const [breedNames, setBreedNames] = useState([]);
+
+    const [filterValue, setFilterValue] = useState({
+        order: "RANDOM",
+        type: "jpg,png,gif",
+        breed_id: '',
+        limit: 5
+    })
+
+    const [fetchBreedNames, isBreedNamesLoading, breedNamesError] = useFetching(async () => {
+        const response = await DogService.getAllBreeds();
+        setBreedNames(response.data);
+    });
+
+    const [fetchUploadedImages, isUploadedImagesLoading, uploadedImagesError] = useFetching(async (filterValue) => {
+        const response = await DogService.getRandomBreed(filterValue);
+        setUploadedImages(response.data);
+    });
+
+    const [fetchLikes, isLikesLoading, likesError] = useFetching(async (file) => {
+        await DogService.uploadImage(file);
+    });
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -21,12 +44,11 @@ const Gallery = () => {
     }
 
     useEffect(() => {
-        fetchLikes(dataFile)
-    }, [dataFile])
+        fetchLikes(dataFile);
+        fetchUploadedImages(filterValue);
+        fetchBreedNames()
+    }, [dataFile, filterValue])
 
-    const [fetchLikes, isLikesLoading, likesError] = useFetching(async (file) => {
-        await DogService.uploadImage(file);
-    });
 
     return (
         <section className={classes.content_wrapper}>
@@ -64,7 +86,7 @@ const Gallery = () => {
                                 </>
                             }
                             {
-                                file && <img src={URL.createObjectURL(file)}/>
+                                file && <img src={URL.createObjectURL(file)} alt="dog"/>
                             }
                     </form>
                     {
@@ -78,13 +100,118 @@ const Gallery = () => {
                     }
                 </MyModal>
             </div>
-            <div className={classes.grid_container}>
-                <div className={classes.div1}></div>
-                <div className={classes.div2}></div>
-                <div className={classes.div3}></div>
-                <div className={classes.div4}></div>
-                <div className={classes.div5}></div>
+            <div className={classes.gallery_group}>
+                <div className={classes.gallery_group_item}>
+                    <label className={classes.gallery_group_title}>Order</label>
+                    <select className={classes.select}
+                            value={filterValue.order}
+                            onChange={(e) =>
+                                setFilterValue({
+                                    order: e.target.value,
+                                    type: filterValue.type,
+                                    breed_id: filterValue.breed_id,
+                                    limit: filterValue.limit,
+                                })}
+                    >
+                        <option value="RANDOM">Random</option>
+                        <option value="ASC">Ascending</option>
+                        <option value="DESC">Descending</option>
+                    </select>
+                </div>
+                <div className={classes.gallery_group_item}>
+                    <label className={classes.gallery_group_title}>Type</label>
+                    <select className={classes.select}
+                            value={filterValue.type}
+                            onChange={(e) =>
+                                setFilterValue({
+                                    order: filterValue.order,
+                                    type:  e.target.value,
+                                    breed_id: filterValue.breed_id,
+                                    limit: filterValue.limit,
+                                })}
+                    >
+                        <option value="jpg, png, gif">All</option>
+                        <option value="jpg, png">Static</option>
+                        <option value="gif">Animated</option>
+                    </select>
+                </div>
+                <div className={classes.gallery_group_item}>
+                    <label className={classes.gallery_group_title}>Breed</label>
+                    <select className={classes.select}
+                            value={filterValue.breed_id}
+                            onChange={(e) =>
+                                setFilterValue({
+                                    order: filterValue.order,
+                                    type: filterValue.type,
+                                    breed_id: e.target.value,
+                                    limit: filterValue.limit,
+                                })}
+                    >
+                        <option value="">None</option>
+                        {
+                            breedNames.map((breedName) =>
+                                <option value={breedName.id}>{breedName.name}</option>
+                            )
+                        }
+                    </select>
+                </div>
+                <div className={classes.gallery_group_item}>
+                    <label className={classes.gallery_group_title}>Limit</label>
+                    <select className={classes.select}
+                            value={filterValue.limit}
+                            onChange={(e) =>
+                                setFilterValue({
+                                    order: filterValue.order,
+                                    type: filterValue.type,
+                                    breed_id: filterValue.breed_id,
+                                    limit: Number(e.target.value),
+                                })}
+                    >
+                        <option value={5}>Limit: 5</option>
+                        <option value={10}>Limit: 10</option>
+                        <option value={15}>Limit: 15</option>
+                        <option value={20}>Limit: 20</option>
+                    </select>
+                </div>
             </div>
+
+            {isUploadedImagesLoading
+                ? <Loader/>
+                :
+                <div className={classes.grid_container}>
+                    {
+                        uploadedImagesError && <h1>Error(</h1>
+                    }
+                    {
+                        uploadedImages && uploadedImages.map(item =>
+                            <div className={classes.container} key={item.id}
+                                // onMouseOver={handleMouseOver}
+                                // onMouseOut={handleMouseOut}
+                            >
+                                {
+                                    item && <img
+                                        src={`${item.url}`}
+                                        alt="breed"
+                                        className={classes.image}
+                                    />
+                                }
+                                {/*{isHovering && (*/}
+                                {/*    <div className={classes.container_hover} onClick={() => router(`/breeds/${item.name}`)}>*/}
+                                {/*        <div className={classes.name_hover}>{item.name}</div>*/}
+                                {/*    </div>*/}
+                                {/*)}*/}
+                            </div>
+                        )
+                    }
+                </div>
+            }
+            {/*<div className={classes.grid_container}>*/}
+            {/*    <div className={classes.div1}></div>*/}
+            {/*    <div className={classes.div2}></div>*/}
+            {/*    <div className={classes.div3}></div>*/}
+            {/*    <div className={classes.div4}></div>*/}
+            {/*    <div className={classes.div5}></div>*/}
+            {/*</div>*/}
         </section>
     );
 };
